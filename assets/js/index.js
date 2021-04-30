0a7b42d14572f14a47d6957996a5e4694f7de385
jjmalfoy <74464264+jjmalfoy@users.noreply.github.com>
2021-04-29 23:07:28 -0400

    Add js and about me

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+
+
+function jumpToElement(element) {
+    if (element != null && typeof element === 'string') {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(element).offset().top
+        }, 500);
+    } else {
+        console.log('Error jumping to element.')
+    }
+
+}
+
+
+$("#parent-button-info").on("click", () => {
+    console.log('Clicked on info...');
+    var nav_button = $("#navigation-button");
+    // jump to projects-section-box
+    
+    if (nav_button.text().includes('INFO')) {
+        jumpToElement("#projects-section-box")
+        // Hide projects box
+        $("#projects-section-box").css("display", "none");
+        // Show about me section
+        $("#about-me-section-box").css("display", "flex")
+        // Change text in navigation-button to Projects
+        $("#navigation-button").html("PROJECTS")
+    } else {
+        jumpToElement("#about-me-section-box")
+        // Show projects box
+        $("#projects-section-box").css("display", "flex");
+        // Hide about me section
+        $("#about-me-section-box").css("display", "none")
+        // Change text in navigation-button to INFO
+        $("#navigation-button").html("INFO")
+    }
+})
+
